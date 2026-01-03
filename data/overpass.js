fed71bb13248d4b7baf81936babe9165ed97c5ee
MQHX <53017981+MQHX@users.noreply.github.com>
2026-01-03 15:07:17 +0100

    Add files via upload

--- data/overpass.js
+++ data/overpass.js
@@ -1,36 +1,36 @@
 const lineups = [
   {
-    id: "monster_from_ct",
+    id: "monster_smoke_from_ct",
     name: "Monster smoke",
     type: "smoke",    // smoke | flash | molotov
 	side: "CT",
-    description: "Left click+Jumpthrow",
-    x: 80.8, // <-- impact (point visible)
+    description: "Left click + Jumpthrow",
+    x: 80.9, // <-- impact (point visible)
     y: 44.6,
 	throw: { // <-- point de lancement
 		x:50.9,
 		y:13.8
 	},
     images: {
-      stand: "images/overpass/monster_from_ct_1.png",
-      aim: "images/overpass/monster_from_ct_2.png"
+      stand: "images/overpass/monster_from_ct_smoke_1.png",
+      aim: "images/overpass/monster_from_ct_smoke_2.png"
     }
   },
   {
-    id: "flash_a",
-    name: "Flash Site A",
-    type: "flash",
+    id: "monster_molly",
+    name: "Monster Molly",
+    type: "molotov",
 	side: "CT",
-    description: "Flash rapide pour entrer sur le site A.",
-    x: 70,
-    y: 30,
+    description: "Left click + Running",
+    x: 80.9,
+    y: 39.7,
 	throw: { // <-- point de lancement
-		x:62.4,
-		y:40.2
+		x:70.9,
+		y:26.5
 	},
     images: {
-      stand: "images/overpass/flash_a_stand.png",
-      aim: "images/overpass/flash_a_aim.png"
+      stand: "images/overpass/monster_molly_1.png",
+      aim: "images/overpass/monster_molly_2.png"
     }
   }
 ];
